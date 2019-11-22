e12d28e56428a2ccd8f28e0d4fee8196a9877216
Terrence Eve <tijayeve@gmail.com>
2019-11-21 21:31:32 -0600

    changed initial state structure

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
-class MyContactFormWithState extends React.Component {
+class MyContactInfoWithState extends React.Component {
+    state = {
+        firstName: "",
+        lastName:"",
+        eMail:"",
+        phone:"",
+        number:"",
+        address:"",
+        city:"",
+        state:"",
+        zipCode:""
+    }
     
+    render () {
+
+        return (
+            <div className="container">
+                <form>
+                    <input type="text" ></input>
+                </form>
+            </div>
+        )
+    }
 }
 
 
@@ -7,6 +28,7 @@ const App = () => {
    return (
        <div>
            <h1>Contacts App</h1>
+           <MyContactInfoWithState />
        </div>
    )
 }
