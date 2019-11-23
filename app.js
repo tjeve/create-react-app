967188d2c7a33ffe3353e9dab440a082c36afefc
Terrence Eve <tijayeve@gmail.com>
2019-11-23 01:21:48 -0600

    updated state with person data

--- app.js
+++ app.js
@@ -3,56 +3,88 @@ class MyContactInfoWithState extends React.Component {
         firstName: "",
         lastName:"",
         eMail:"",
-        phone:"",
-        number:"",
+        phoneNumber:"",
         address:"",
         city:"",
         state:"",
         zipCode:"",
         people: []
     }
-    
-    handleSubmit = (submitEvent) => {
+    //handles all changes to the form and updates the state. 
+    handleInputChange = (changeEvent) => {
+        console.log(changeEvent.target.name, changeEvent.target.value)
+        this.setState({
+            [changeEvent.target.name]: changeEvent.target.value
+        })
+    }
+
+    handleFormSubmit = (submitEvent) => {
         submitEvent.preventDefault() //prevents your console logs from immediately disappearing
-        alert("Contact Information was submitted:" + this.state.people)
-        console.log(this.state)
+
         const newPeople = [...this.state.people]
         newPeople.push({
-            people: newPeople
+            people: newPeople,
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            eMail: this.state.eMail,
+            phoneNumber:this.state.phoneNumber,
+            address:this.state.address,
+            city: this.state.city,
+            state: this.state.state,
+            zipCode:this.state.zipCode,
         })
-        console.log(this.people)
+
+        this.setState({
+            people: newPeople,
+            firstName: "",
+            lastName:"",
+            eMail:"",
+            phoneNumber:"",
+            address:"",
+            city:"",
+            state:"",
+            zipCode:""
+        })
     }
 
     render () {
-
+        console.log("People:" + this.state.people)
         return (
             <div className="container">
-                <form onSubmit={ this.handleSubmit }>
+                <form onSubmit={ this.handleFormSubmit }>
                     <div className="form-group">
                         <input type="text" name="firstName"
-                        placeholder="First Name" 
+                        placeholder="First Name"
+                        onChange={this.handleInputChange}
                         value={this.state.firstName}></input>
                         <input type="text" name="lastName" 
-                        placeholder="Last Name"></input>
+                        placeholder="Last Name"
+                        onChange={this.handleInputChange}></input>
                     </div>
                     <div className="form-group">
                         <input type="text" name="eMail"
-                        placeholder="Email"></input>
-                        <input type="text" name="phone Number"
-                        placeholder="Phone"></input>
+                        placeholder="Email"
+                        onChange={this.handleInputChange}></input>
+                        <input type="text" name="phoneNumber"
+                        placeholder="Phone"
+                        onChange={this.handleInputChange}></input>
                     </div>
                     <div className="form-group">
                         <div>
                             <input type="text" name="address"
-                            placeholder="Address"></input>
+                            placeholder="Address"
+                            onChange={this.handleInputChange}></input>
                             <input type="text" name="city"
-                            placeholder="City"></input>
+                            placeholder="City"
+                            onChange={this.handleInputChange}></input>
                         </div>
                         <div>
                             <input type="text" name="state"
-                            placeholder="State"></input>
+                            placeholder="State"
+                            onChange={this.handleInputChange}></input>
                             <input type="text" name="zipcode"
-                            placeholder="Zipcode"></input>
+                            placeholder="Zipcode"
+                            onChange={this.handleInputChange}></input>
                         </div>
                     </div>
                     <input type="submit"></input>
